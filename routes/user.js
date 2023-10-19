844195b7d49ccb15b2e35ee8ec5c2b6d16755874
John Edelbi <iceflaming@gmail.com>
2023-10-19 18:52:18 +0200

    create user route

--- routes/user.js
+++ routes/user.js
@@ -1,24 +1,8 @@
 import express from 'express';
+import userController from '../controllers/user.js';
 const userRoutes = express.Router();
 
-// set cookie and session
-userRoutes.get('/', (req, res) => {
-    req.session.userName = 'JohnEdelbi';
-    res.cookie('name', 'john', { maxAge: 5 * 60 * 1000 });
-    res.send('hello');
-});
-
-//get cookie & session
-userRoutes.get('/verify', (req, res) => {
-    const { name } = req.cookies;
-    console.log(name);
-    console.log(req.session.userName);
-    if (req.session && req.session.userName && name) {
-        const userName = req.session.userName;
-        res.send(`hello ${name} your username is: ${userName}`);
-    } else {
-        res.send('session or cookie is not valid');
-    }
-});
+userRoutes.get('/', userController.setCookieAndSession);
+userRoutes.get('/verify', userController.verifyUserCookieAndSession);
 
 export default userRoutes;
